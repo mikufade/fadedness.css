dd9593972cc9fae35fbbec120239be8f4438b940
mikufade <mikufade@hotmail.com>
2024-03-12 16:34:03 +0800

    feat: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default  {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSameLine: false,
+  arrowParens: 'always',
+  singleAttributePerLine: true
+}
